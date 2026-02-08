d80193828cd299e4cf174db6f9e82b9570d87bc4
Deepanshu <deepanshu2024@lpu.in>
2026-02-08 17:24:23 +0530

    Added Some Routes for Student

--- routes/students.js
+++ routes/students.js
@@ -1,21 +1,67 @@
 import { Router } from "express";
 import { isStudentValid } from "../schemas/validations";
 import { Student, Attendance } from "./schemas/db";
-import z from 'zod';
+import bcrypt from 'bcrypt';
+import { config } from "dotenv";
 import jwt from 'jsonwebtoken';
-
+import mongoose from "mongoose";
+config();
 
 
 export const studentRouter = Router();
 
-studentRouter.post('login', (req,res)=>{
-    const isValid = isStudentValid.safeParse(req.body);
-    if(!isValid){
-        return res.status(400).send("Invalid in Input");
+const jwtpass = process.env.STUDENTJWT;
+
+const authMiddleware = async (req, res, next)=>{
+    try{
+        const authToken = req.headers.authorization;
+        if(!authToken.startsWith("Bearer ")){
+            return res.status(401).send("Authorization token missing");
+        }
+        const token = authToken.split(" ")[1];
+        const payload = jwt.verify(token, jwtpass);
+        const vid = payload.vid;
+        const student = await Student.findOne({vid});
+        if(!student){
+            return res.status(404).send("Student is not enrolled");
+        }
+        req.vid = vid;
+        next();
     }
-    const {name, password, fullName, vid} = req.body;
-    const student = Student.findOne({vid});
+    catch(err){
+        next(err);
+    }
+}
+
+studentRouter.post('/login', async (req,res, next)=>{
+    try{
+    const {vid, password} = req.body;
+    const student = await Student.findOne({vid});
     if(!student){
         return res.status(404).send("Student Not Enrolled");
     }
-})
+    const match = await bcrypt.compare(password, student.password);
+    if(!match){
+        return res.status(400).send("Incorrect Password, Try Again");
+    }
+    const token = jwt.sign({id : student._id ,vid: vid}, jwtpass);
+    res.status(200).json({message: "Login Successful", jwt: token});
+    }
+    catch(err){
+        next(err);
+    }
+
+});
+
+studentRouter.use(authMiddleware);
+
+studentRouter.get('/AttendanceSummary', async (req,res,next)=>{
+    try{
+        const vid = req.vid;
+        const data = await Attendance.findOne({vid});
+        res.status(200).json(data);
+    }
+    catch(err){
+        next(err);
+    }
+});
--- schemas/validations.js
+++ schemas/validations.js
@@ -1,8 +1,3 @@
 import z from 'zod';
 
-export const isStudentValid = z.object({
-    email : z.email().endsWith('@lpu.in'),
-    password: z.string().min(8),
-    fullName: z.string(),
-    vid: z.number()
-})
+export const isPasswordValid = z.string().min(8).regex(/[A-Z]/, "Must contain UpperCase Letter").regex(/[0-9]/, "Must Contain a number");
